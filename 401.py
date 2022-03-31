d7a632182d7cb917d5c270811db5dcef927eff9c
Mountainsub <70077254+mathematic-master@users.noreply.github.com>
2022-04-01 06:26:22 +0900

    Create 401.py

---
+++ 401.py
@@ -0,0 +1,21 @@
+#temp.values
+temp2 = np.datetime64(datetime.datetime.now(), "m")
+print(temp2)
+
+import pandas as pd
+temp = datetime.datetime(2022, 4, 1, 6, 23 )
+
+np.datetime64(temp)
+
+import numpy as np
+import datetime
+import time
+import asyncio
+temp = np.datetime64(temp)
+now= np.datetime64(datetime.datetime.now(), "s")
+while  now <= temp:
+    print("not time")
+    asyncio.sleep(1)
+    now = np.datetime64(datetime.datetime.now(), "s")
+else:
+    print("No problem")
